02e361e40ba9687c713cffe968d837a2b38f760e
Akshay Sharma <akshay.sharma03@sap.com>
2021-05-31 18:02:39 +0530

    Lexical_Scope Explanation Added

--- Lexical_Scopping.js
+++ Lexical_Scopping.js
@@ -1,7 +1,7 @@
 function sayHi()
 {
-    this.firstanem ="Akshauy";
-    console.log((this.firstanem));
+    this.firstname ="Akshay";
+    console.log((this.firstname));
     function sayHello()
     {
         console.log(this.firstanem);
@@ -10,3 +10,6 @@ function sayHi()
 }
 
 sayHi();
+
+// Lexical scope is the ability for a function scope to access variables from the parent scope.
+// We call the child function to be lexically bound by that of the parent function.
