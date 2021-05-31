d95fb973bfed46ee1bf45459444f4ce4c0c87f86
Akshay Sharma <akshay.sharma03@sap.com>
2021-05-31 17:28:56 +0530

    CLosures Explanation Added

--- closures.js
+++ closures.js
@@ -1,14 +1,18 @@
+// What Closure is?
+// A function bundlled with its lexical scope is known as Closure, Whenever the function is returenned by using return statememt in Closures, The
+// function still holds the closure binding. 
+
 function sayHi()
 {
-    this.firstanem ="Akshauy";
+    this.firstname ="Akshay";
     this.lastname ="Sharma";
-    console.log((this.firstanem));
+    console.log((this.firstname));
+    console.log(a);
     function sayHello()
     {
         console.log(this.lastname);
      }
-    return sayHello;
-     
+    return sayHello; // returning the function sayHello
 }
 
 var x =sayHi();
