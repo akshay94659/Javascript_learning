cd48596f69b298e0c8a26f0e90c50cfddcd0fa0e
Akshay Sharma <akshay.sharma03@sap.com>
2021-06-01 18:17:10 +0530

    fill&filter.js Explanation Updated

--- fill&filter.js
+++ fill&filter.js
@@ -1,12 +1,13 @@
 // arrya.fill("Value", start o index, end of index)
 
 // It is used to fill the array values with the array function
-var arrays = ["Akshay", "Sharma", "IS","A","Good"];
+// Includes the Start index but exclusive of the end index
 
+var arrays = ["Akshay", "Sharma", "IS","A","Good"];
 arrays.fill("h",2,3);
 
 console.log(arrays);
 
-//filter sis used to find the value in the array
+//filter is used to find the value in the array
 
 console.log(arrays.filter((e)=> e!="h"))
