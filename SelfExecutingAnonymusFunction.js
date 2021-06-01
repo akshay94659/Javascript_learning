645ea8d013bf02a8b203f11bbe58abe95b4349ed
Akshay Sharma <akshay.sharma03@sap.com>
2021-06-01 21:55:10 +0530

    Files Updated

--- SelfExecutingAnonymusFunction.js
+++ SelfExecutingAnonymusFunction.js
@@ -6,7 +6,6 @@
 
 // This is called the Self Executing Anonymus function in javascript but it also called IIFE also.
 
-
 // The key point of this function is that you do not need to call the function itself it is getting executed itself.
 
 // Used in Web Scrapping and Automation Testing 
