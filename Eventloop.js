f57f271fa9c144073794f5ea2ed6ce106fcb1e88
Akshay Sharma <akshay.sharma03@sap.com>
2021-06-01 18:14:55 +0530

    Eventloop.js Explanation Updated

--- Eventloop.js
+++ Eventloop.js
@@ -1,7 +1,11 @@
+//Arrow function with no Input Parameters
+
 const incidents =()=>{
     console.log("Hi, i got two incicents");
 
 }
+
+// Arrow Functions with setTimeout function in itself 
 
 var solution = ()=>{
     setTimeout(()=>
@@ -13,13 +17,19 @@ var solution = ()=>{
 
 const more_incidents = ()=>
 {
-    console.log("hi, I got twp more incidnets");
+    console.log("hi, I got two more incidnets");
 
 }
 
 incidents();
 solution();
 more_incidents();
+
+//Output 
+// Hi, i got two incicents
+// Hi, I have solved one Incident
+// hi, I got two more incidnets
+// Hi, Mehul have been assigned
 
 // This signifies that in Javascript, the function will not work as synchorn but rather will work as async
 // This is used for functions with heavy load required.
